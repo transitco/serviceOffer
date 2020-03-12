bbfba65798e42c820c2e23caecd56bfbd538da20
Francis Peixoto <peixoto.francis@gmail.com>
2020-03-12 16:00:12 -0400

    removed mongo call for now

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const cors = require('cors');
 server.use(express.json());
 server.use(cors());
 
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to database!'));
-
-
 server.get("/", (req, res) => {
     res.send("Hello World!");
 })
