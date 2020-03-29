4ec1f4e3913e4d017ea6563e2a53f38fe7ac4caf
cptcloud <sven.diebold@gmail.com>
2020-03-29 12:23:37 -0400

    Fix query resolver
    Make it generic so we can query stm or exo data.

--- index.js
+++ index.js
@@ -14,20 +14,26 @@ db.once('open', function() {
 
 const resolvers = {
   Query: {
-    routes: async () => {
+    routes: async (_, {agency_key, route_id}) => {
       const test_route = await gtfs.getRoutes({
-        agency_key: 'exo_gtfs',
-        route_id: '6'
+        agency_key: agency_key,
+        route_id: route_id
       });
       console.log(test_route);
       return test_route;
       },
   },
   // 3
-  Route: {
+  route: {
     route_id: (parent) => parent.route_id,
     route_short_name: (parent) => parent.route_short_name,
     route_long_name: (parent) => parent.route_long_name,
+    route_desc: (parent) => parent.route_desc,
+    route_url: (parent) => parent.route_url,
+    route_color: (parent) => parent.route_color,
+    route_text_color: (parent) => parent.route_text_color,
+    trip_count: (parent) => parent.trip_count,
+    pattern_count: (parent) => parent.pattern_count,
   }
 }
 
