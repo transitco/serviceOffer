197281c272d83d7d294d1f544d4584a7862ec088
cptcloud <sven.diebold@gmail.com>
2020-05-01 08:09:35 -0400

    First mutation
    It works!
    
    Change-Id: Id5c443091849846fa750ab786de3da02bbe2aab6

--- src/graphql/resolvers/mutations.resolvers.js
+++ src/graphql/resolvers/mutations.resolvers.js
@@ -1,8 +1,24 @@
 /* eslint-disable no-unused-vars */
 var gtfs = require('gtfs');
+<<<<<<< HEAD
 
 const mutations_resolvers = {
  
 }
+=======
+const Agency = require('../../../node_modules/gtfs/models/gtfs/agency');
+
+const mutations_resolvers = {
+  Mutation: {
+    agency: async (parent, args, ctx, info) => {
+      if (typeof args.agency_new_name === "string") {
+        var query = {'agency_key': args.agency_key};
+        const db_agency = await Agency.findOneAndUpdate(query, { agency_name: args.agency_new_name });
+        return db_agency;
+      }
+    },
+  },
+};
+>>>>>>> 4bfe24e... First mutation
 
 module.exports = mutations_resolvers
