616a81b421d891eafe16285d188f9814b1cc8f8e
cptcloud <sven.diebold@gmail.com>
2020-05-04 21:20:25 -0400

    Replaced graphql-yoga per apollo
    apollo-server-express stack is widely adopted by the community.
    Many advantages of graphq-yoga are support by apollo.
    
    Change-Id: I8ccefac472ce021c37627dedc9b92ed0909ed13b

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { importSchema } = require('graphql-import');
 
-const {GraphQLServer} = require('graphql-yoga');
 const mongoose = require('mongoose');
 const queries_resolvers = require('./graphql/resolvers/queries.resolvers')
 const mutations_resolvers = require('./graphql/resolvers/mutations.resolvers')
+
 const resolvers = [queries_resolvers, mutations_resolvers];
+
 mongoose.connect('mongodb://mongo:27017/gtfs',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
 );
-const server = new GraphQLServer({
-  typeDefs: './src/graphql/schemas/schema.graphql',
+
+const server = new ApolloServer({
+  typeDefs: importSchema('./src/graphql/schemas/schema.graphql'),
   resolvers,
 });
+
+const app = express();
+server.applyMiddleware({ app });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  server.start(() => console.log(`GraphQL API Server is running on http://localhost:4000`));
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
 });
